4ae504901fbf3efe872da8fbaa8f81641dfa877e
Aditya Goswami <akgoswami185@gmail.com>
2025-06-26 21:45:31 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,31 +1,17 @@
-from setuptools import setup, find_packages
+from setuptools import setup
 
 setup(
-    name='reconveritas',
+    name='ReconVeritas',
     version='1.0.0',
-    author='Aditya Goswami',
-    author_email='your-email@example.com',
-    description='A complete recon automation suite for offensive security assessments.',
-    long_description=open('README.md').read(),
-    long_description_content_type='text/markdown',
-    url='https://github.com/Aditya-Sec/ReconVeritas-Automated-Recon-Tool',
-    packages=find_packages(),
-    include_package_data=True,
     py_modules=['recon-suite'],
-    install_requires=[
-        'requests',
-        'dnspython'
-    ],
     entry_points={
         'console_scripts': [
-            'reconveritas = recon-suite:main'
-        ]
+            'reconveritas = recon-suite:main',
+        ],
     },
-    classifiers=[
-        'Programming Language :: Python :: 3',
-        'License :: OSI Approved :: MIT License',
-        'Operating System :: Linux',
-        'Topic :: Security',
+    install_requires=[
+        'requests',
+        'beautifulsoup4',
+        'colorama'
     ],
-    python_requires='>=3.6',
 )
